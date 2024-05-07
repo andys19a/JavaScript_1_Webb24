da07d458bd9edf6d9061c4c84f8b397cf8ace95f
andys19a <159772694+andys19a@users.noreply.github.com>
2024-05-07 10:13:05 +0200

    ovning_9

---
+++ övning/André_ovning/js/script.js
@@ -0,0 +1,15 @@
+let array = [5,5,5];
+
+let sum = 0
+
+for (let i = 0; i < array.length; i++){
+    sum += array [i]
+}
+
+console.log(sum)
+
+let myName = "Andre";
+let age = 27;
+console.log (myName + age )
+
+console.log (`${myName} ${age}`);
